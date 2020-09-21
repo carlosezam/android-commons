6271e45eceeca4e5476cffc1088cb17c39cdc6a0
CarlosEzam <carlos.ezam@gmail.com>
2020-09-21 15:08:09 -0500

    set jvm 1.8

--- app/src/main/java/com/carlosezam/commons/android/sample/MainActivity.kt
+++ app/src/main/java/com/carlosezam/commons/android/sample/MainActivity.kt
@@ -2,12 +2,17 @@ package com.carlosezam.commons.android.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import com.carlosezam.commons.android.coroutines.DefaultDispatcherProvider
 import com.carlosezam.commons.android.delegates.IntPreferenceDelegate
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
     }
 }
