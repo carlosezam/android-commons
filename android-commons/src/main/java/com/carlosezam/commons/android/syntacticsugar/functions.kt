c8c5f8afb98ee858f54570c6dd4b00d6cc208f00
CarlosEzam <carlos.ezam@gmail.com>
2020-12-04 13:01:36 -0500

    Add tryOrNull and tryOrNullSuspend

--- android-commons/src/main/java/com/carlosezam/commons/android/syntacticsugar/functions.kt
+++ android-commons/src/main/java/com/carlosezam/commons/android/syntacticsugar/functions.kt
@@ -8,3 +8,21 @@ import kotlin.contracts.contract
 fun unless(predicate: Boolean, block: () -> Unit ) : Unit {
     if( !predicate ) block()
 }
+
+fun <T> tryOrNull(
+    function : () -> T
+) : T? =
+    try {
+        function()
+    } catch (e: Exception) {
+        null
+    }
+
+suspend fun <T> tryOrNullSuspend(
+    function: suspend () -> T
+) : T?  =
+    try {
+        function()
+    } catch (e: Exception) {
+        null
+    }
