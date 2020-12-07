b17c6636673ee873650203b2d8553d317d051973
CarlosEzam <carlos.ezam@gmail.com>
2020-12-07 14:38:18 -0500

    Add log helper extensions

---
+++ android-commons/src/main/java/com/carlosezam/commons/android/extensions/Log.kt
@@ -0,0 +1,18 @@
+package com.carlosezam.commons.android.extensions
+
+import android.util.Log
+
+inline val <reified T> T.TAG: String
+    get() = T::class.java.simpleName
+
+inline fun <reified T> T.logd(message: String) = Log.d(TAG, message)
+
+inline fun <reified T> T.loge(message: String) = Log.e(TAG, message)
+
+inline fun <reified T> T.logi(message: String) = Log.i(TAG, message)
+
+inline fun <reified T> T.logv(message: String) = Log.v(TAG, message)
+
+inline fun <reified T> T.logw(message: String) = Log.w(TAG, message)
+
+inline fun <reified T> T.logwtf(message: String) = Log.wtf(TAG, message)
